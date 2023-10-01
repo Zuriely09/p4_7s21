369f2c14a40af0105abb13b7ba277c5fc3762bc6
jenniferzuriely <jenniferzurienriquez09@gmail.com>
2023-09-30 21:08:11 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/isc/tesoem/CEJZ/p4_7s21/ContestaActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.isc.tesoem.CEJZ.p4_7s21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContestaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contesta);
+    }
+}
